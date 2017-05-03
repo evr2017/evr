133eb50961f70b18c69bc0bd35e2f65fbcd32928
Karen Matias <matiaskarenkay@gmail.com>
2017-05-03 18:47:42 +0400

    Finalized Discover Main Page

--- app/src/main/java/evr/com/evr/utils/Stubs.java
+++ app/src/main/java/evr/com/evr/utils/Stubs.java
@@ -15,9 +15,12 @@ public class Stubs {
     public static List<DiscoverSection> getDiscoverSections(){
         List<DiscoverSection> discoverSections = new ArrayList<>();
 
-        discoverSections.add(new DiscoverSection("Rooms", R.drawable.sample_image));
-        discoverSections.add(new DiscoverSection("Restaurant", R.drawable.sample_image));
-        discoverSections.add(new DiscoverSection("Entertainment", R.drawable.sample_image));
+        discoverSections.add(new DiscoverSection("Food and Dining", R.drawable.menu_1));
+        discoverSections.add(new DiscoverSection("Active and Outdoor", R.drawable.menu_2));
+        discoverSections.add(new DiscoverSection("Events", R.drawable.menu_3));
+        discoverSections.add(new DiscoverSection("Accommodation", R.drawable.menu_4));
+        discoverSections.add(new DiscoverSection("Museum", R.drawable.menu_5));
+        discoverSections.add(new DiscoverSection("Parks", R.drawable.menu_6));
 
         return discoverSections;
     }
