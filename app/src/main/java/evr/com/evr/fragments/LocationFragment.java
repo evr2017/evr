8b148be2b0e65021a8769905ce44426f4f7ab8d7
Jerome Catalon <jerome.w.catalon@accenture.com>
2017-05-15 01:20:01 +0800

    Added Google Map with static marker (global one)

--- app/src/main/java/evr/com/evr/fragments/LocationFragment.java
+++ app/src/main/java/evr/com/evr/fragments/LocationFragment.java
@@ -1,22 +1,42 @@
 package evr.com.evr.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.ContentLoadingProgressBar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import evr.com.evr.R;
 
 /**
  * Created by karenmatias on 30/04/2017.
  */
 
-public class LocationFragment extends Fragment {
+public class LocationFragment extends Fragment implements OnMapReadyCallback {
 
-    public LocationFragment() {
-    }
+
+    @BindView(R.id.mapView)
+    protected MapView mapView;
+
+    private GoogleMap googleMap;
+
+    @BindView(R.id.map_loading_progress)
+    protected ContentLoadingProgressBar progressBar;
+
+    private LatLng globalOne = new LatLng(14.608595, 121.081609);
 
     /**
      * Returns a new instance of this fragment for the given section
@@ -29,10 +49,76 @@ public class LocationFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_location, container, false);
-        TextView textView = (TextView) rootView.findViewById(R.id.textView1);
-        textView.setText(getClass().getSimpleName());
-        return rootView;
+
+        View view = inflater.inflate(R.layout.fragment_location, container, false);
+        ButterKnife.bind(this, view);
+
+        mapView.onCreate(savedInstanceState);
+
+        if (mapView != null) {
+            mapView.getMapAsync(this);
+        }
+
+        return view;
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        this.googleMap = googleMap;
+        this.googleMap.getUiSettings().setMyLocationButtonEnabled(false);
+        this.googleMap.addMarker(new MarkerOptions().position(globalOne)).setVisible(true);
+
+        // Needs to call MapsInitializer before doing any CameraUpdateFactory calls
+        MapsInitializer.initialize(this.getActivity());
+
+        // Updates the location and zoom of the MapView
+        CameraPosition cameraPosition = new CameraPosition.Builder().
+                target(globalOne).
+                tilt(45).
+                zoom(17.5f).
+                build();
+        this.googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
 }
