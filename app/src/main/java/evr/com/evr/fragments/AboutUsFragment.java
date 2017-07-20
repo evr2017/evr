e13f736d67770b9673a37e8b7fdbcc96465e9d8a
Jerome Catalon <jerome.w.catalon@accenture.com>
2017-07-20 16:35:36 +0800

    Updated firebase url references

--- app/src/main/java/evr/com/evr/fragments/AboutUsFragment.java
+++ app/src/main/java/evr/com/evr/fragments/AboutUsFragment.java
@@ -1,6 +1,7 @@
 package evr.com.evr.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,24 +22,28 @@ public class AboutUsFragment  extends Fragment {
     }
 
     public static Fragment newInstance() {
-        Fragment fragment = new AboutUsFragment();
-        return fragment;
+        return new AboutUsFragment();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_about, container, false);
-        TextView textView = (TextView) rootView.findViewById(R.id.textView1);
+        /*TextView textView = (TextView) rootView.findViewById(R.id.textView1);
         textView.setText(getClass().getSimpleName());
-
         WebView myWebView = (WebView) rootView.findViewById(R.id.webView1);
         myWebView.loadUrl("http://192.168.43.111:8887");
         WebSettings webSettings=myWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        return rootView;
+        webSettings.setJavaScriptEnabled(true);*/
+        return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
     }
+
+
 }
 
 
